8675fe52318b5641d3ed160ba6917225ec2c0d92
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-12-31 23:43:31 +0100

    ArgonAssault

--- ArgonAssault1/Assets/Scripts/Enemy.cs
+++ ArgonAssault1/Assets/Scripts/Enemy.cs
@@ -4,9 +4,51 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private void OnParticleCollision(GameObject other)
+    [SerializeField] GameObject deathFX;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 2;
+
+    GameObject ParentGameObject;
+    ScoreBoard scoreBoard;
+
+    void Start()
     {
-        Debug.Log($"I am hit by {other.gameObject.name}");
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        ParentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        RigidBody();
+    }
+
+    void RigidBody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+        
+    }
+
+    void KillEnemy()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+        GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = ParentGameObject.transform;
         Destroy(gameObject);
+        
+    }
+
+    void ProcessHit()
+    {
+        GameObject fx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        fx.transform.parent = ParentGameObject.transform;
+        hitPoints --;
+        
     }
 }
---
+++ ArgonAssault1/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        int numMusic = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusic > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
